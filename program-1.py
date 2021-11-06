f3612866127c8131a000811352a91cac6fc04e53
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-06 19:55:01 +0800

    Code completed.

--- program-1.py
+++ program-1.py
@@ -1,5 +1,9 @@
 def getNameAgeAddress():
-    user_name = input("Enter your name: ")
+    user_name = input("\nEnter your name: ")
     user_age = int(input("Enter your age: "))
     user_address = input("Enter your address: ")
     return user_name, user_age, user_address
+
+name, age, address = getNameAgeAddress()
+
+print (f"\nHi, my name is {name}. I am {age} years old and I live in {address}. \n")
