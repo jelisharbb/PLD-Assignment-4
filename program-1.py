80a7fc9feb31a492cb432ca20c2b54d377fd5570
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-06 19:49:18 +0800

    Defined a function.

---
+++ program-1.py
@@ -0,0 +1,5 @@
+def getNameAgeAddress():
+    user_name = input("Enter your name: ")
+    user_age = int(input("Enter your age: "))
+    user_address = input("Enter your address: ")
+    return user_name, user_age, user_address
