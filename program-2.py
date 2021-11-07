e61cf8209e6607712b88f3932e6519e382246da5
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-07 13:35:39 +0800

    Defined a function.

---
+++ program-2.py
@@ -0,0 +1,4 @@
+ def getQuantity():
+     appleQuan = int(input("How many apples do you want to purchase? "))
+     orangeQuan = int(input("How many oranges do you want to purchase? "))
+     return appleQuan, orangeQuan
