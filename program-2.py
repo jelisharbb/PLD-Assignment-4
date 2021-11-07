fe2add99792a96961422e7c7cce0e7bd3d814079
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-07 14:28:59 +0800

    Code completed.

--- program-2.py
+++ program-2.py
@@ -1,4 +1,15 @@
- def getQuantity():
-     appleQuan = int(input("How many apples do you want to purchase? "))
-     orangeQuan = int(input("How many oranges do you want to purchase? "))
-     return appleQuan, orangeQuan
+print ("\nWelcome to Fruit Market! \nHere's our pricelist: \n> Apples = 20 PHP \n> Orange = 25 PHP \n")
+ 
+def getQuantity():
+    appleQuan = int(input("How many apple/s do you want to purchase? "))
+    orangeQuan = int(input("How many orange/s do you want to purchase? "))
+    grandTtl = fruitPrc(apple=appleQuan, orange=orangeQuan)
+    print (f"The total amount is {grandTtl:,.2f} PHP.")
+
+def fruitPrc(apple, orange):
+    applePrc = 20 * apple
+    orangePrc = 25 * orange
+    fruitTtl = applePrc + orangePrc
+    return fruitTtl;
+
+getQuantity()
