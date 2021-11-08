ae6ca00e0fac59865384643bb2db159c56ad0c86
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-08 19:52:07 +0800

    Defined the first function.

---
+++ program-3.py
@@ -0,0 +1,5 @@
+print ("\nWelcome to Apple Store!")
+
+def appleDtls ():
+    userMoney = float(input("How much money do you have? "))
+    applePrc = float(input("How much is the cost of an apple each? "))
