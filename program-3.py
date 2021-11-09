4b25e2bd1babb81aa44a39084d33b3e5105ce690
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-09 14:28:02 +0800

    Added if function.

--- program-3.py
+++ program-3.py
@@ -1,5 +1,16 @@
 print ("\nWelcome to Apple Store!")
 
 def appleDtls ():
-    userMoney = float(input("How much money do you have? "))
+    userMny = float(input("How much money do you have? "))
     applePrc = float(input("How much is the cost of an apple each? "))
+    return userMny, applePrc
+
+user_money, apple_prc = appleDtls()
+
+def userRcpt (money, apple):
+    if money >= apple:
+        receipt = money % apple
+        appleQntty = money // apple
+        print (f"You can buy {appleQntty} and you have a change of {receipt:,.2f} PHP.")
+
+    
